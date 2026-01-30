ea92fd968d1a9c3f4ba0be951275b65e2ccc61b2
Israel Soto Cala <israelsotoprogramador@outlook.com>
2026-01-30 20:26:07 +0100

    Mascaras

--- Scripts/control.gd
+++ Scripts/control.gd
@@ -5,15 +5,26 @@ signal se_ha_ido
 @onready var cliente: TextureRect = $Cliente
 var puede_interactuar: bool = false
 
+@onready var mascaras: TextureRect = $Cliente/Mascaras
+
+
 func _ready() -> void:
 	var centro_x = get_viewport_rect().size.x / 2
 	
 	cliente.global_position.x = -cliente.size.x 
 	
+	mascaras.global_position.x = -mascaras.size.x
+	
 	var tween_entrada = create_tween()
+	var tween_entrada_mascaras = create_tween()
 	var destino_final = centro_x - (cliente.size.x / 2)
+	var destino_final_mascaras = centro_x - (mascaras.size.x / 2)
 	
 	tween_entrada.tween_property(cliente, "global_position:x", destino_final, 0.8)\
+		.set_trans(Tween.TRANS_QUAD)\
+		.set_ease(Tween.EASE_OUT)
+		
+	tween_entrada_mascaras.tween_property(mascaras, "global_position:x", destino_final_mascaras, 0.8)\
 		.set_trans(Tween.TRANS_QUAD)\
 		.set_ease(Tween.EASE_OUT)
 	
@@ -40,3 +51,7 @@ func _on_boton_no_pressed() -> void:
 		se_ha_ido.emit()
 		queue_free()
 	)
+
+func _generar_mascara() -> void:
+	print("Peruano")
+	mascaras.texture = load("res://ruta/imagen.png")
