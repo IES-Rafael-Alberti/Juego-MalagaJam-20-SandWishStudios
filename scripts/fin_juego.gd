04e209477e3d4d2a8386c821ee1b8d47535a9a58
Jusm8 <migacota@gmail.com>
2026-01-31 13:12:41 +0100

    cambio leve

--- scripts/fin_juego.gd
+++ scripts/fin_juego.gd
@@ -2,13 +2,13 @@ extends Control
 
 @onready var reiniciar: TextureButton = $TextureRect/Reiniciar
 @onready var salir: TextureButton = $TextureRect/Salir
+@onready var puntuacionLb: Label = $TextureRect/puntuacionLb
 
 func _ready() -> void:
 	pass
 
 func _on_reiniciar_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
-	
 
 func _on_salir_pressed() -> void:
 	get_tree().quit()
