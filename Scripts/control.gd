a1cef18ca0149fa07dd60febf5ba74ea6308efce
Jesús Cordero Ávila <74388155+JesCorAvi@users.noreply.github.com>
2026-01-31 20:33:40 +0100

    ajustes barra radial

--- Scripts/control.gd
+++ Scripts/control.gd
@@ -4,6 +4,7 @@ signal se_ha_ido
 
 @onready var cliente: TextureRect = $Cliente
 @onready var nodo_mascara_visual: TextureRect = $Cliente/Mascaras 
+@onready var progress_bar_radial: TextureProgressBar = $TextureProgressBar
 
 var puede_interactuar: bool = false
 var mascarasDict: Dictionary = {}
@@ -11,18 +12,14 @@ var categoria_actual: String = ""
 var mascara_categoria: String = ""
 var esvip: bool
 
-# Variables para la cola
 var en_cola: bool = false
 var color_original: Color = Color.WHITE
 
-# Variables para recordar la posición Y original
 var orig_y_cliente: float
 var orig_y_mascaras: float
 
 var tiempo_maximo: float = 5.0 
-# --- NUEVA VARIABLE DE VELOCIDAD ---
 var duracion_anim: float = 0.5
-# -----------------------------------
 
 @export var mascaras_mexicanas : Array[MascaraData]
 @export var mascaras_tiki : Array[MascaraData]
@@ -37,6 +34,8 @@ var duracion_anim: float = 0.5
 
 func _ready() -> void:
 	tiempo_limite.wait_time = tiempo_maximo
+	
+	_configurar_barra_radial()
 	
 	var random_idx = randi_range(1, 5)
 	var textura_pj = load("res://Assets/pj/pj%d.png" % random_idx)
@@ -80,8 +79,6 @@ func _ready() -> void:
 		var destino_cola_masc_y = orig_y_mascaras + offset_altura
 		
 		var tween = create_tween().set_parallel(true)
-		
-		# Usamos duracion_anim ajustada para la cola (un poco más lento que el centro)
 		var tiempo_cola = duracion_anim * 1.6
 		
 		tween.tween_property(cliente, "global_position:x", destino_cola, tiempo_cola)\
@@ -100,6 +97,46 @@ func _ready() -> void:
 	else:
 		_animar_entrada_al_centro(destino_final, destino_final_mascaras)
 
+func _process(_delta: float) -> void:
+	if puede_interactuar and not en_cola and is_instance_valid(tiempo_limite):
+		var tiempo_restante = tiempo_limite.time_left
+		var porcentaje = (tiempo_restante / tiempo_limite.wait_time) * 100
+		
+		progress_bar_radial.value = porcentaje
+		progress_bar_radial.visible = true
+		
+		progress_bar_radial.global_position = cliente.global_position + Vector2(-10, -10)
+		
+		if porcentaje > 50:
+			progress_bar_radial.tint_progress = Color.SPRING_GREEN
+		elif porcentaje > 25:
+			progress_bar_radial.tint_progress = Color.GOLD
+		else:
+			progress_bar_radial.tint_progress = Color.TOMATO
+	else:
+		progress_bar_radial.visible = false
+
+func _configurar_barra_radial() -> void:
+	var gradiente = Gradient.new()
+	gradiente.offsets = [0.0, 0.8, 0.85, 1.0]
+	gradiente.colors = [Color.WHITE, Color.WHITE, Color.TRANSPARENT, Color.TRANSPARENT]
+	
+	var tex = GradientTexture2D.new()
+	tex.gradient = gradiente
+	tex.fill = GradientTexture2D.FILL_RADIAL
+	tex.fill_from = Vector2(0.5, 0.5)
+	tex.width = 64
+	tex.height = 64
+	
+	progress_bar_radial.texture_under = tex
+	progress_bar_radial.texture_progress = tex
+	
+	progress_bar_radial.tint_under = Color(0, 0, 0, 0.4) 
+	progress_bar_radial.fill_mode = TextureProgressBar.FILL_CLOCKWISE
+	progress_bar_radial.min_value = 0
+	progress_bar_radial.max_value = 100
+	progress_bar_radial.custom_minimum_size = Vector2(50, 50)
+
 func avanzar_al_centro() -> void:
 	en_cola = false
 	var centro_x = get_viewport_rect().size.x / 2
@@ -112,7 +149,6 @@ func avanzar_al_centro() -> void:
 func _animar_entrada_al_centro(dest_x, dest_masc_x) -> void:
 	var tween = create_tween().set_parallel(true)
 	
-	# Usamos la variable duracion_anim
 	tween.tween_property(cliente, "global_position:x", dest_x, duracion_anim)\
 		.set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 	tween.tween_property(nodo_mascara_visual, "global_position:x", dest_masc_x, duracion_anim)\
@@ -164,13 +200,10 @@ func _on_boton_si_pressed() -> void:
 	if not puede_interactuar: return
 	
 	if mascara_categoria == categoria_actual:
-		print("BIEN ")
 		aumentar_puntuacion()
-		
 		if esvip:
 			get_parent().multiplicador += inc_vip
 	else:
-		print(" MAL ")
 		reducir_puntuacion(1)
 	
 	_animar_salida(get_viewport_rect().size.x + 100)
@@ -179,11 +212,8 @@ func _on_boton_no_pressed() -> void:
 	if not puede_interactuar: return
 
 	if mascara_categoria != categoria_actual:
-		print("BIEN ")
 		aumentar_puntuacion()
-		
 	else:
-		print("MAL")
 		reducir_puntuacion(1)
 
 	_animar_salida(-cliente.size.x - 100)
@@ -192,7 +222,6 @@ func _animar_salida(destino_x: float) -> void:
 	puede_interactuar = false
 	var tween = create_tween().set_parallel(true)
 	
-	# Usamos la variable duracion_anim
 	tween.tween_property(cliente, "global_position:x", destino_x, duracion_anim)\
 		.set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
 	tween.tween_property(nodo_mascara_visual, "global_position:x", destino_x, duracion_anim)\
@@ -205,18 +234,15 @@ func _animar_salida(destino_x: float) -> void:
 	)
 
 func _on_tiempo_limite_timeout() -> void:
-	print("Se acabó el tiempo")
 	reducir_puntuacion(2)
 	_animar_salida(-cliente.size.x - 100) 
 	
 func aumentar_puntuacion():
 	get_parent().puntuacion += aumento
 	get_parent().registrar_acierto()
-	print(aumento)
 	
 func reducir_puntuacion(valor: int):
 	if valor == 1:
 		get_parent().puntuacion += reduccion
 	else:
 		get_parent().puntuacion += ausencia
-	print(reduccion)
