8a2226d407606ec4027969b7f5c2233dcc53344c
Jesús Cordero Ávila <74388155+JesCorAvi@users.noreply.github.com>
2026-01-31 17:08:33 +0100

    integracion

--- scripts/notes.gd
+++ scripts/notes.gd
@@ -7,11 +7,7 @@ extends Control
 @onready var estado_3: TextureRect = $Estado3
 @onready var estado_4: TextureRect = $Estado4
 
-# Labels
-@onready var estado_1_lb: Label = $Estado1/Estado1Lb
-@onready var estado_2_lb: Label = $Estado2/Estado2Lb
-@onready var estado_3_lb: Label = $Estado3/Estado3Lb
-@onready var estado_4_lb: Label = $Estado4/Estado4Lb
+
 
 func _ready() -> void:
 	pass 
@@ -26,22 +22,15 @@ func actualizar_estado(tipo_actual: String) -> void:
 	estado_3.modulate = rojo
 	estado_4.modulate = rojo
 
-	estado_1_lb.modulate = rojo
-	estado_2_lb.modulate = rojo
-	estado_3_lb.modulate = rojo
-	estado_4_lb.modulate = rojo
+
 
 	# Ahora activamos el correcto en verde
 	match tipo_actual:
 		"tiki":
 			estado_1.modulate = verde
-			estado_1_lb.modulate = verde
 		"japon":
 			estado_2.modulate = verde
-			estado_2_lb.modulate = verde
 		"carnaval":
 			estado_3.modulate = verde
-			estado_3_lb.modulate = verde
 		"mexicanas":
 			estado_4.modulate = verde
-			estado_4_lb.modulate = verde
