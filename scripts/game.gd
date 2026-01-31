e2b0fec398aa9faeb9f0ae6124499238d1ea2e9e
Jusm8 <migacota@gmail.com>
2026-01-31 01:36:03 +0100

    timer y progres bar bien

--- scripts/game.gd
+++ scripts/game.gd
@@ -9,6 +9,8 @@ var instancia_actual = null
 
 var categoria_global: String = ""
 var categoria_pendiente: String= ""
+var timeout_pausado := false
+
 
 func _ready() -> void:
 	randomize()
@@ -25,7 +27,10 @@ func _process(delta: float) -> void:
 	actTimerCambio()
 
 func actTimerCambio() -> void:
-	progress_bar.value = timer_cambio.time_left
+	if timeout_pausado:
+		progress_bar.value = 0
+	else:
+		progress_bar.value = timer_cambio.time_left
 
 func generarInvitado():
 	if categoria_pendiente != "":
@@ -48,15 +53,27 @@ func generarInvitado():
 
 func dejarPasar():
 	if is_instance_valid(instancia_actual):
+		_rearmar_timer_si_timeout()
 		instancia_actual._on_boton_si_pressed()
 
 func dejarSalir():
 	if is_instance_valid(instancia_actual):
+		_rearmar_timer_si_timeout()
 		instancia_actual._on_boton_no_pressed()
 		
 func cambioFiesta():
+	timeout_pausado = true
+	timer_cambio.stop()
+	progress_bar.value = 0
+	
 	if is_instance_valid(instancia_actual):
 		categoria_pendiente = instancia_actual.obtener_otra_categoria(categoria_global)
 		print("¡Cambio de fiesta pendiente! Siguiente invitado será: ", categoria_pendiente)
 
 	timer_cambio.start()
+
+func _rearmar_timer_si_timeout() -> void:
+	if timeout_pausado:
+		timeout_pausado = false
+		progress_bar.value = timer_cambio.wait_time
+		timer_cambio.start()
